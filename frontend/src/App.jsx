60a4e239ddf6b01409102652741b8250718f9342
Lena Perišić <110422715+lenaperisic@users.noreply.github.com>
2026-01-04 19:18:35 +0100

    app.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,13 @@
 import './style/base.css';
 
 import { useEffect, useState } from "react";
+import LoginPage from './loginPage';
 
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:5000/hello")
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-      .catch(err => console.error("Greška:", err));
-  }, []);
-
   return (
-    <div style={{ margin: "40px", fontSize: "20px" }}>
-      <h1>React + Flask demo</h1>
-      <p>{message}</p>
+    <div>
+      <LoginPage />
     </div>
   );
 }
