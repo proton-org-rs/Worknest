9517319301d1b437973a51d75b3613f19e06bbc3
krickovicx <krickovic.milica@gmail.com>
2026-01-07 11:44:11 +0100

    add title to reservation

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -6,17 +6,18 @@ api = Blueprint("api", __name__)
 def hello():
     return jsonify({"message": "Pozdrav sa Flask backend-a!"})
 
-@app.route('/api/reservations-calendar', methods=['GET'])
+@api.route('/api/reservations-calendar', methods=['GET'])
 def get_reservations():
     return jsonify(RESERVATIONS)
 
-@app.route('/api/reservations-calendar', methods=['POST'])
+@api.route('/api/reservations-calendar', methods=['POST'])
 def create_reservation():
     data = request.json
-    reservation = {
+    reservation  = {
         "id": str(uuid.uuid4()),
+        "title": data.get("title", "No title"),
         "start": data["start"],
         "end": data["end"]
     }
-    RESERVATIONS.append(reservation)
-    return jsonify(reservation), 201
+    RESERVATIONS.append(reservation )
+    return jsonify(reservation ), 201
