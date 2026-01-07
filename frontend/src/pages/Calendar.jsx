1c458a9c31ef724929966f00902622d75c85a429
krickovicx <krickovic.milica@gmail.com>
2026-01-07 11:46:43 +0100

    edit calendar style

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -5,42 +5,67 @@ import interactionPlugin from '@fullcalendar/interaction'
 import { useEffect, useState } from 'react'
 
 export default function CalendarPage() {
-  const [reservations, setReservations] = useState([])
+    const [reservations, setReservations] = useState([])
 
-  useEffect(() => {
-    fetch('/api/reservations-calendar')
-      .then(res => res.json())
-      .then(data => setReservations(data))
-  }, [])
+    useEffect(() => {
+        fetch('/api/reservations-calendar')
+        .then(res => res.json())
+        .then(data => setReservations(data))
+    }, [])
 
-  const handleDateSelect = (selectInfo) => {
-    const title = prompt('Task name:')
-    if (!title) return
+    const handleDateSelect = (selectInfo) => {
+        const title = prompt('Task name:')
+        if (!title) return
 
-    const newReservation = {
-      title,
-      start: selectInfo.startStr,
-      end: selectInfo.endStr
+        const newReservation = {
+            title, 
+            start: selectInfo.startStr,
+            end: selectInfo.endStr
+        }
+
+        fetch('http://localhost:3000/api/reservations-calendar', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newReservation)
+        })
+        .then(async res => {
+            
+            // For debugging
+            // (creating a new reservation throws an error)
+
+            const text = await res.text()
+            alert('Server response: '+text)  
+            return JSON.parse(text)                 
+        })
+        .then(saved => {
+            setReservations(prev => [...prev, saved])
+            alert(`Saved new event: ${saved.title} ${saved.startStr} - ${saved.endStr}`)
+        })
+        .catch(err => {
+            console.error('Error saving event', err)
+            alert('Error saving event: ' + err)
+        })
+
     }
 
-    fetch('/api/reservations-calendar', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newReservation)
-    })
-      .then(res => res.json())
-      .then(saved => setReservations([...reservations, saved]))
-  }
-
-  return (
-    <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      selectable
-      editable
-      events={reservations}
-      select={handleDateSelect}
-      height="auto"
-    />
-  )
+    return (
+        <div style={{ padding: '20px', height: '90vh' }}>
+        <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        allDaySlot={false}
+        nowIndicator={true}
+        slotDuration="01:00:00"
+        slotMinTime="06:00:00"       
+        slotMaxTime="22:00:00"
+        selectable
+        editable
+        firstDay="1"
+        events={reservations}
+        select={handleDateSelect}
+        expandRows={true}
+        height="100%"
+        />
+        </div>
+    )
 }
