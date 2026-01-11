9b8940d041f7fb7d91f4c57b20da6bfbed3ba937
Veljko Milic <milicvoff@gmail.com>
2026-01-11 18:53:49 +0100

    dodat POST endpoint za rezervaciju sala

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 
 api = Blueprint("api", __name__)
 
 @api.route("/hello")
 def hello():
     return jsonify({"message": "Pozdrav sa Flask backend-a!"})
+
+@api.route("/rooms/<int:room_id>/reserve", methods=["POST"])
+def reserve_room(room_id):
+    data = request.get_json() #uzmi JSON iz bodyja
+
+    if not data:
+        return jsonify({"error": "Invalid data"}), 400 # moze i druga poruka za error
+    
+    required_fields = ["date", "start_time", "end_time", "description"]
+    for field in required_fields:
+        if field not in data:
+            return jsonify({"error": f"Missing field: {field}"}), 400 #HTTP kod za Bad Request
+        
+    return jsonify({
+        "message": "Reservation request received!",
+        "room_id": room_id,
+        "data": data
+    }), 201 #HTTP kod 2xx za uspesnu realizaciju
